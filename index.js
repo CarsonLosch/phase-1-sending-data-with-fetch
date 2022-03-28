1ab99c35a514138d2765805b5c4fe06ecd77de71
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-03-27 18:49:18 -0700

    commit tree e6f7ea82d385e6dbc4368b9e44ba1f00494cd13f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+submitData = function(userName, userEmail) {
+    return fetch(`http://localhost:3000/users`, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            Accept: "application/json",
+        },
+        body:JSON.stringify({name: userName, email: userEmail}),
+    })
+    .then(res => res.json())
+    .then(userData => document.querySelector('p').textContent = userData.id)
+    .catch(function(error){
+        alert('Something is wrong here!!!');
+        return document.querySelector('p').textContent = error.message
+    })
+}
